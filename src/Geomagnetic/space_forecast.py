aa4aa7a730fa9cb5478119bf6c961ef076bdea93
jjedd97 <jedonnelly97@yahoo.ca>
2022-12-08 09:55:41 -0700

    forecasting data

--- src/Geomagnetic/space_forecast.py
+++ src/Geomagnetic/space_forecast.py
@@ -1,3 +1,27 @@
+import logging
+
+import pandas as pd
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+from lxml import etree
+
+from src.utils.dataframe_functions import create_dataframe_for_x_days
+
+
 ### This file will be used to fetch and parse data from: https://www.spaceweather.gc.ca
 # In this data source Calgary is considered to be in the Sub-Auroral Zone
 
+def get_27_day_forecast():
+    '''
+    url = "https://www.spaceweather.gc.ca/forecast-prevision/long/sflt-1-en.php"
+    long_term_url="/forecast-prevision/long/sflt-en.php"
+    response = requests.get(url)
+    '''
+    url = "https://www.spaceweather.gc.ca/forecast-prevision/long/sflt-1-en.php"
+    long_term_url = "/forecast-prevision/long/sflt-en.php"
+    tables = pd.read_html(url+long_term_url)  # Returns list of all tables on page
+    forecast_table = tables[0]
+    logging.debug(f" forecast returned: {forecast_table}")
+    return forecast_table
